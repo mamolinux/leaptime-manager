4cf90afc584b413e35432b17b39783c15f0f8e45
Himadri Sekhar Basu <41947504+hsbasu@users.noreply.github.com>
2024-10-26 23:32:54 +0530

    Prepare for scheduled backup (#87)
    - d/control: use python3

---
+++ src/LeaptimeManager/scheduler.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2021-2023 Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
+# 
+# This file is part of LeapTime Manager.
+# 
+# LeapTime Manager is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# LeapTime Manager is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with LeapTime Manager. If not, see <http://www.gnu.org/licenses/>
+# or write to the Free Software Foundation, Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA 02110-1301, USA..
+# 
+# Author: Himadri Sekhar Basu <hsb10@iitbbs.ac.in>
+#
+
+# import the necessary modules!
+import gettext
+import locale
+import logging
+
+
+# imports from current package
+from LeaptimeManager.cli_args import  APP, LOCALE_DIR
+
+# i18n
+locale.bindtextdomain(APP, LOCALE_DIR)
+gettext.bindtextdomain(APP, LOCALE_DIR)
+gettext.textdomain(APP)
+_ = gettext.gettext
+
+# logger
+module_logger = logging.getLogger('LeaptimeManager.scheduler')
